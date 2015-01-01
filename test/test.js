d400a0c22ff628bcb4af1802b65b6552c877f06a
Khaliq Gant <khaliqgant@gmail.com>
2015-01-01 23:49:41 +0100

    add in tests and remove fake json config file

--- test/test.js
+++ test/test.js
@@ -3,15 +3,26 @@
 
 /**
  * RemotEE Tests
- * @use run suite of tests simulating cli arguments and fake config file
+ * @use run suite of tests simulating cli arguments
  */
 
 var should = require('should'),
+    shell = require('shelljs'),
     assert = require('assert'),
-    remotee = require('../bin/remotee-sync.js');
+    remotee = '../bin/remotee-sync.js';
 
 require('mocha');
 
-describe('remotee-sync', function(){
+describe('RemotEE-Sync Tests', function(){
+    it('should pass by throwing an error because no options were passed', function(done){
+        assert.equal("You must provide ssh information to correctly connect to a remote server",
+        shell.exec(remotee).output.replace(/[\n\t\r]/g,""));
+        done();
+    });
 
+    it('should pass by throwing an error because no location was set to save the db', function(done){
+        assert.equal("You set for the database to save but didn't specify a location to save the db. DB dump will be saved in the root of the project",
+        shell.exec(remotee + ' --ssh=test-staging -s').output.replace(/[\n\t\r]/g,""));
+        done();
+    });
 });
