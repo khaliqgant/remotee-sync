cde5226f37a988c3aa4112da7cf59f7ba0e39f3e
Khaliq Gant <khaliqgant@gmail.com>
2015-01-01 22:24:14 +0100

    add in asset requirement

--- test/test.js
+++ test/test.js
@@ -2,15 +2,16 @@
 'use strict';
 
 /**
- * RemotEE main.js Test
+ * RemotEE Tests
  * @use run suite of tests simulating cli arguments and fake config file
  */
 
 var should = require('should'),
+    assert = require('assert'),
     remotee = require('../bin/remotee-sync.js');
 
 require('mocha');
 
 describe('remotee-sync', function(){
 
-)};
+});
